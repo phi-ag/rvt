476d42e1b1039718acb9920dcda68c440c6e3543
Peter Hirn <peter.hirn@phi.ag>
2025-05-05 17:38:49 +0200

    wip: more element table hacking

--- src/elementTable.test.ts
+++ src/elementTable.test.ts
@@ -1,25 +1,40 @@
 import { describe, expect, test } from "vitest";
 
 import { elementTable } from "./elementTable.js";
+import { openPath } from "./node.js";
 import { adskExampleFile, exampleFile } from "./node.test.js";
 
 describe("element table", () => {
-  test("empty-2025", async () => {
+  test.skip("empty-2025", async () => {
     const file = await exampleFile("empty-2025.rvt");
     const table = await elementTable(file);
 
     expect(table.size).toEqual(2413);
   });
 
-  test("empty-2026", async () => {
+  test.skip("empty-2026", async () => {
     const file = await exampleFile("empty-2026.rvt");
     const table = await elementTable(file);
 
     expect(table.size).toEqual(2426);
   });
 
-  test("2026", async () => {
+  test.skip("2021", async () => {
+    const file = await adskExampleFile("racbasicsamplefamily-2021.rfa");
+    const table = await elementTable(file);
+
+    expect(table.size).toEqual(1814);
+  });
+
+  test.skip("2026", async () => {
     const file = await adskExampleFile("racbasicsamplefamily-2026.rfa");
+    const table = await elementTable(file);
+
+    expect(table.size).toEqual(1992);
+  });
+
+  test.skip("large-proprietary", async () => {
+    const file = await openPath("/home/peter/rdp/008_YYYY_MOD_F10_Z01-2021.rvt");
     const table = await elementTable(file);
 
     expect(table.size).toEqual(1992);
--- src/elementTable.ts
+++ src/elementTable.ts
@@ -3,10 +3,11 @@ import * as array from "./utils/array.js";
 
 export interface ElementEntry {
   id: bigint;
-  unknown1: number;
-  unknown2: number;
-  unknown3: number;
-  unknown4: bigint;
+  u1: number;
+  u2: number;
+  u3: number;
+  u4: bigint;
+  u5: number;
 }
 
 export interface ElementTable {
@@ -25,13 +26,98 @@ const decompress = async (data: Uint8Array): Promise<Uint8Array> => {
   const decompressedStream = stream.pipeThrough(new DecompressionStream("gzip"));
 
   const chunks = [];
-  for await (const chunk of decompressedStream) {
-    chunks.push(chunk);
+  try {
+    for await (const chunk of decompressedStream) {
+      chunks.push(chunk);
+    }
+  } catch (e) {
+    if (e instanceof Error && (e.cause as { errno: number })?.errno === -3) {
+      console.warn("Expected invalid gzip data in ElemTable");
+    } else {
+      throw e;
+    }
   }
 
   const blob = new Blob(chunks);
   const buffer = await blob.arrayBuffer();
-  return new Uint8Array(buffer);
+  //return new Uint8Array(buffer);
+  const foo = new Uint8Array(buffer);
+  //console.log("ASDFASF", foo.length);
+  return foo;
+  /*
+  const stream = new DecompressionStream("gzip");
+  try {
+    await stream.writable.getWriter().write(data);
+  } catch (e) {
+    console.log("ASDF", e);
+  }
+
+  const result = await stream.readable.getReader().read();
+  if (!result.value) throw Error("SDAF");
+
+  console.log("ASDFASF", result.value.length);
+
+  return result.value;
+  */
+
+  //console.log(await stream.readable.getReader().read()); // "Hello World"
+};
+
+const elementIdSize = (flag: number): 32 | 64 => {
+  switch (flag) {
+    case 0x04:
+      return 32;
+    case 0x05:
+      return 64;
+  }
+
+  throw Error(`Unexpected element table id size (${flag})`);
+};
+
+const tableEntry32 = (view: DataView, offset: number): ElementEntry => {
+  const id = view.getInt32(offset, true);
+  const id2 = view.getInt32(offset + 4, true);
+  const u1 = view.getInt32(offset + 8, true);
+  const u2 = view.getInt32(offset + 12, true);
+  const u3 = view.getInt32(offset + 16, true);
+  const u4 = view.getInt32(offset + 20, true);
+  const u5 = view.getInt32(offset + 24, true);
+
+  // NOTE: found a large 2021 project where this throws
+  if (id !== id2) throw Error(`Id mismatch (${id} != ${id2})`);
+  // NOTE: found a large 2021 project where this throws
+  if (u4 !== 0) throw Error(`Unknown4 is not zero (${id}) ${u5}`);
+
+  return {
+    id: BigInt(id),
+    u1,
+    u2,
+    u3,
+    u4: BigInt(u4),
+    u5
+  };
+};
+
+const tableEntry64 = (view: DataView, offset: number): ElementEntry => {
+  const id = view.getBigInt64(offset, true);
+  const u1 = view.getInt32(offset + 8, true);
+  const u2 = view.getInt32(offset + 12, true);
+  const u3 = view.getInt32(offset + 16, true);
+  const id2 = view.getBigInt64(offset + 20, true);
+  const u4 = view.getBigInt64(offset + 28, true);
+  const u5 = view.getInt32(offset + 36, true);
+
+  if (id !== id2) throw Error(`Id mismatch (${id} != ${id2})`);
+  if (u5 !== 0) throw Error(`Unknown5 is not zero (${id})`);
+
+  return {
+    id,
+    u1,
+    u2,
+    u3,
+    u4,
+    u5
+  };
 };
 
 export const parseElementTable = async (data: Uint8Array): Promise<ElementTable> => {
@@ -42,8 +128,7 @@ export const parseElementTable = async (data: Uint8Array): Promise<ElementTable>
   const decompressed = await decompress(data.subarray(8));
   const fileVersion = decompressed[0];
 
-  if (decompressed[1] !== 0x05)
-    throw Error(`Unexpected element table decompressed header [${decompressed[1]}]`);
+  const idSize = elementIdSize(decompressed[1]);
 
   const view = new DataView(
     decompressed.buffer,
@@ -52,41 +137,31 @@ export const parseElementTable = async (data: Uint8Array): Promise<ElementTable>
   );
 
   const size = view.getInt32(2, true);
+  console.log("Size", size);
+  console.log("View", view.buffer.byteLength);
+
+  const chunkSize = idSize === 32 ? 28 : 40;
+  console.log("chunks", (view.buffer.byteLength - 6) / chunkSize);
 
   const ids: ElementEntry[] = [];
   for (let i = 0; i < size; i++) {
-    const offset = i * 40 + 6;
-    const id = view.getBigInt64(offset, true);
-    const unknown1 = view.getInt32(offset + 8, true);
-    const unknown2 = view.getInt32(offset + 12, true);
-    const unknown3 = view.getInt32(offset + 16, true);
-    const id2 = view.getBigInt64(offset + 20, true);
-    const unknown4 = view.getBigInt64(offset + 28, true);
-    const unknown5 = view.getInt32(offset + 36, true);
+    const offset = i * chunkSize + 6;
 
-    if (id !== id2) throw Error(`Id mismatch (${id} != ${id2})`);
-    if (unknown5 !== 0) throw Error(`Unknown5 is not zero (${id})`);
-
-    ids.push({
-      id,
-      unknown1,
-      unknown2,
-      unknown3,
-      unknown4
-    });
+    const entry = idSize === 32 ? tableEntry32(view, offset) : tableEntry64(view, offset);
+    ids.push(entry);
+    //if (entry.id === BigInt(size)) break;
+    //throw Error("ASDFASFASDF");
   }
 
-  console.log(size);
+  console.log(ids.slice(0, 200));
+
+  /*
+  console.log(ids.slice(0, 100).map((e) => e.id));
   //const missing = [1521n, 1957n, 1967n, 1976n, 2283n];
   const missing = [17n, 23n, 24n, 25n, 29n, 31n, 32n, 33n, 5702n, 5701n, 7199n, 7200n];
   //console.log(ids.slice(0, 100).map((e) => e.id));
 
   console.log(ids.filter((e) => e.id === 0n));
-  /*
-  console.log(ids.filter((e) => e.id === 1n));
-  console.log(ids.filter((e) => e.id === 1519n));
-  console.log(ids.filter((e) => e.id === 1520n));
-  */
   console.log(ids.filter((e) => missing.includes(e.id)));
 
   console.log(ids.filter((e) => e.id > e.unknown4).length);
@@ -109,6 +184,7 @@ export const parseElementTable = async (data: Uint8Array): Promise<ElementTable>
   console.log(ids.filter((e) => e.id <= e.unknown4));
   console.log(ids.find((e) => e.id === 1530n));
   console.log(ids.find((e) => e.id === 1527n));
+  */
 
   //const foo = ids.filter((e) => e.id > e.unknown4);
   //writeFileSync("ids.txt", foo.map((e) => e.id).join(","));
