285ae6f01b5923d0309336effb7cdfd837787908
Peter Hirn <peter.hirn@phi.ag>
2025-05-05 18:02:51 +0200

    tests: add deno

--- vitest.config.ts
+++ vitest.config.ts
@@ -2,6 +2,7 @@ import { defineConfig } from "vitest/config";
 
 export default defineConfig({
   test: {
+    include: ["src/**/*.test.ts"],
     reporters: ["default", "junit"],
     outputFile: {
       junit: "junit.xml"
